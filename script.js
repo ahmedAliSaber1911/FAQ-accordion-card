186fe5b5c20f7ddbd9316ca84726d6f92864991b
Ahmed Saber <ahmedalisaber965@gmail.com>
2022-08-28 16:57:10 +0200

    finish the chellenge

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-var ahmed = document.getElementsByClassName('question');
-
+let questions = document.querySelectorAll(".question");
+questions.forEach(question =>{question.addEventListener("click", ()=>{
+    document.querySelector(".answer").style.display = "block";
+    document.querySelector(".questionTitle").style.fontWeight = 700 ;
+})})
